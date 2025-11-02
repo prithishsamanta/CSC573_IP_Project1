4eab438c22e6ad439b6842481a4dd7b2f6781ae4
prithishsamantae <prithishsamanta@gmail.com>
2025-11-01 21:17:57 -0400

    Added Basic StatusCode , Peer Info File

--- src/main/java/org/p2p/common/PeerInfo.java
+++ src/main/java/org/p2p/common/PeerInfo.java
@@ -1,4 +1,27 @@
 package org.p2p.common;
 
 public class PeerInfo {
+    private final String host;
+    private final int uploadPort;
+
+    public PeerInfo(String host, int uploadPort) {
+        this.host = host;
+        this.uploadPort = uploadPort;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getUploadPort() {
+        return uploadPort;
+    }
+
+    @Override
+    public String toString() {
+        return "PeerInfo{" +
+                "host='" + host + '\'' +
+                ", uploadPort=" + uploadPort +
+                '}';
+    }
 }
